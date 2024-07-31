bbfeff76911772595fe66c310d9c0e73731eceff
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-01 01:57:46 +0900

    削除処理実装2

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-# pin "@rails/ujs", to: "rails-ujs.js" #npmを使用するためコメントアウト
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
