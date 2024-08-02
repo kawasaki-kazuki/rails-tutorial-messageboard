d2a10c00461ac6862b30dcc34c2d7a72cf62f898
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-02 23:21:15 +0900

    編集画面で保存失敗してもエラー表示されるようにした - fix:保存失敗の処理はエラーハンドラーとしてまとめた

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,23 +3,15 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    # messegeを全て取得する
     @messages = Message.all
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path, notice: 'メッセージを保存しました'
     else
-      #メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました"
-      # render 'index'　←_form.html.erbでTurboを無効にしているのでindexをレンダリングできないのでコメントアウトした
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('form_messages', partial: 'form', locals: { message: @message }) }
-        format.html { render :index }
-      end
+      handle_save_error(:index)
     end
   end
 
@@ -28,26 +20,33 @@ class MessagesController < ApplicationController
 
   def update
     if @message.update(message_params)
-      #保存に成功した場合はトップメッセージへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path, notice: 'メッセージを編集しました'
     else
-      # 保存に失敗した場合は編集画面へ戻す
-      render 'edit'
+      handle_save_error(:edit)
     end
   end
 
   def destroy
     @message.destroy
-    redirect_to root_path , notice: 'メッセージを削除しました'
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
 
   private
-  
+
   def message_params
     params.require(:message).permit(:name, :body)
   end
 
   def set_message
     @message = Message.find(params[:id])
+  end
+
+  def handle_save_error(action)
+    @messages = Message.all
+    flash.now[:alert] = "メッセージの保存に失敗しました"
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace('form_messages', partial: 'form', locals: { message: @message }) }
+      format.html { render action }
+    end
   end
 end
